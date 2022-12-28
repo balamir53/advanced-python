b79262418b8278b37ce67d845f5458a57888ec16
balamir53 <balamir53@yahoo.com>
2022-12-28 10:00:43 +0300

    02-flask-smorest

---
+++ 02-flask-smorest/app.py
@@ -0,0 +1,51 @@
+import uuid
+
+from flask import Flask, request
+
+from db import stores, items
+
+app = Flask(__name__)
+
+
+@app.get('/store')
+def get_stores():
+    return {'stores': list(stores.values())}
+
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
+
+
+# we have to create unique ids for each store
+@app.post('/store')
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+
+    #
+    # for store in stores:
+    #     if store["name"] == name:
+    #         new_item = {"name": request_data["name"], "price": request_data["price"]}
+    #         store["items"].append(new_item)
+    #         return new_item
+    return {"message": "Store not found"}, 404

efbe0c8a70f8d36a583104249a9dfc197be8c9fb
balamir53 <balamir53@yahoo.com>
2022-12-29 19:28:55 +0300

    04-jwt

--- 04-jwt/schemas.py
+++ 04-jwt/schemas.py
@@ -45,3 +45,8 @@ class ItemUpdateSchema(Schema):
     price = fields.Float()
 
 
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
+

16869e45fb9aa18a15eea05c2c7e0a3b93770890
balamir53 <balamir53@yahoo.com>
2022-12-29 11:57:25 +0300

    03-sqlalchemy

--- 03-sqlachemy/app.py
+++ 03-sqlachemy/app.py
@@ -6,6 +6,8 @@ from db import db
 import models
 from resources.item import blp as ItemBluePrint
 from resources.store import blp as StoreBluePrint
+from resources.tag import blp as TagBluePrint
+
 
 
 def create_app(db_url=None):
@@ -30,5 +32,6 @@ def create_app(db_url=None):
 
     api.register_blueprint(ItemBluePrint)
     api.register_blueprint(StoreBluePrint)
+    api.register_blueprint(TagBluePrint)
 
     return app
